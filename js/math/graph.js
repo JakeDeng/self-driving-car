51f668c7eeb496945d654352d0fcad6db83ab669
JakeDeng <31949617+JakeDeng@users.noreply.github.com>
2024-01-05 07:34:07 +0000

    add point and segment button and functionality

--- js/math/graph.js
+++ js/math/graph.js
@@ -4,6 +4,26 @@ class Graph {
     this.segments = segments;
   }
 
+  addPoint(point) {
+    if(this.containsPoint(point)) return false;
+    this.points.push(point);
+    return true;
+  }
+
+  addSegment(segment) {
+    if(this.containsSegment(segment)) return false;
+    this.segments.push(segment);
+    return true;
+  }
+
+  containsPoint(point) {
+    return this.points.find(p => p.equals(point)) != null;
+  }
+
+  containsSegment(segment) {
+    return this.segments.find(s => s.equals(segment)) != null;
+  }
+
   draw(ctx){
     for(const segment of this.segments){
       segment.draw(ctx);
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 myCanvas.width = 600;
 myCanvas.height = 600;
 
@@ -9,3 +10,23 @@ const seg1 = new Segment(p1, p2);
 
 const graph = new Graph([p1, p2], [seg1]);
 graph.draw(ctx);
+
+function addRandomPoint() {
+  const x = Math.random() * myCanvas.width;
+  const y = Math.random() * myCanvas.height;
+  graph.addPoint(new Point(x, y));
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+  graph.draw(ctx);
+}
+
+function addRandomSegment() {
+  const index1 = Math.floor(Math.random() * graph.points.length);
+  const index2 = Math.floor(Math.random() * graph.points.length);
+  
+  if(index1 != index2) {
+    const seg = new Segment(graph.points[index1], graph.points[index2]);
+    graph.addSegment(seg);
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    graph.draw(ctx);
+  }
+}
